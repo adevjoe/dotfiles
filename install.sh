49845f9a4052cab72d30afd91aaf9b56fdc07aaa
Joe <adevjoe@gmail.com>
2019-05-26 18:20:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ main() {
   fi
   echo 'source $HOME/.st/.source' >> $ZSHRC
   echo 'source $HOME/.st/.vimrc' >> $VIMRC
-  /bin/bash -c 'source $ZSHRC'
+  /bin/bash -c "source $HOME/.st/.source"
 
   printf "${GREEN}"
   echo ''
